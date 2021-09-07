4b914d67260f983c6cebc9e03031a05a6900cde1
mohdafnas <mohdafnas7@gmail.com>
2021-09-07 22:19:01 +0530

    new

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
+import { WorksComponent } from './works/works.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SampleComponent,
+    WorksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
